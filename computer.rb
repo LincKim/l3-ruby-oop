561466f513317d17e4e5bda8e7d62157658cfefb
LincKim <lincolnkimutai@gmail.com>
2023-02-15 11:05:32 +0300

    More lessons

--- computer.rb
+++ computer.rb
@@ -1,6 +1,6 @@
 # super class
 class Computer
-    attr_Accessor :model, :country, :year
+    attr_accessor :model, :country, :year
     
     def initialize(model, country, year)
         @model = model
@@ -9,11 +9,35 @@ class Computer
     end
     
     def print_computer
-        puts "Computer: #{self.model}, Year: #{self.year}"
+        puts "Computer: #{self.model}, Year: #{self.year}, Country: #{self.country}"
     end
 
 end
 
 # TODO: Implement class that inherits from Computer class
+class Dell < Computer    # This is inherits everything from the Computer class 
+                         # As showm below in the code.
+end
+
+xps = Dell.new('XPS 13', 'USA', 2022)
+xps.print_computer
+
+
+class Lenovo < Computer     # Remember the syntax for inheritance
+
+    def print_computer
+        puts 'This is better than the other one!'
+    end
+
+    # def original_print_computer      # Hasn't quite worked out
+    #     super.print_computer
+
+    # end
+end
+
+yog = Lenovo.new('Yog 13', 'USA', 2022)
+yog.print_computer
+# yog.original_print_computer
+
 
 # TODO: Use the super keyword to access attributes and methods from Computer
--- plant.rb
+++ plant.rb
@@ -1,4 +1,32 @@
 # TODO: Implement a class do illustrate how class variables and methods work.
 
+class Plant 
+
+    attr_accessor :plant_name, :plant_species
+
+    @@count = 0
+
+    def initialize(plant_name, plant_species)
+        @plant_name = plant_name
+        @plant_species = plant_species
+        @@count += 1  # adds by 1 everytime you create a new object 
+    end
+
+    def self.count
+        @@count
+    end
 
 
+end
+
+
+mango = Plant.new('Mango', 'Maembe')
+puts Plant.count
+avocado = Plant.new('Avocado', 'Parachichi')
+puts Plant.count
+apple = Plant.new('Apple', 'Tufaha')
+passion = Plant.new('Passion', 'Pass')
+watermelon = Plant.new('Watermelon', 'Tikitimaji')
+pineapple = Plant.new('Pineapple', 'Nanasi')
+
+puts Plant.count
